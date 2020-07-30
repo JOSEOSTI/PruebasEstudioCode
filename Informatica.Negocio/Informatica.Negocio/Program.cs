a4d6de5b1b40ad06c98fe5cd404508f077571fe7
jose <jao.jarama@yavirac.edu.ec>
2020-07-30 17:32:09 -0500

    cambios de prueba realizados en lenguaje oriendato a objetos

---
+++ Informatica.Negocio/Informatica.Negocio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Informatica.Negocio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
